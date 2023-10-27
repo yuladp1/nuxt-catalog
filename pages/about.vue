54079dd70c2419a2f5611943df2c82f0b6061ff6
yuladp1 <yuladp1@gmail.com>
2023-10-27 20:50:44 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>First nuxt project, about page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ut sequi enim veniam illum cumque voluptate eveniet iste dignissimos, eaque, facere minima, possimus tenetur commodi delectus debitis laboriosam et suscipit.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
